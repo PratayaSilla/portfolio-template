30addcbcec90ccac20e2a174fe02a8af434c2ae5
Prataya <pratayasilla@gmail.com>
2025-05-06 16:13:57 +0530

    Contact section upadation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import './Contact.css'
+import "./Contact.css"
 
 function Contact() {
   return (
-    <div className='contact-container'>
-        <div className="contact-header">
-            <h1 className="contact-title">CONTACT</h1>
-            <div className="contact-divider"></div>
+    <div className="contact-container">
+      <div className="contact-header">
+        <h2>CONTACT</h2>
+        <div className="header-line"></div>
+      </div>
+
+      <div className="contact-content">
+        <div className="contact-text">
+          <p>Looking to start a project or you</p>
+          <p>need help from me?</p>
+          <p>feel free to reach out to me</p>
         </div>
+
+        <div className="contact-form">
+          <div className="form-row">
+            <input type="text" placeholder="First name" className="input-field" />
+            <input type="text" placeholder="Last name" className="input-field" />
+          </div>
+          <textarea placeholder="Message..." className="message-field"></textarea>
+          <button className="message-button">Message</button>
+        </div>
+      </div>
     </div>
   )
 }
